fc20a7e5e63cbd42238745bc2a3e9b55c791960d
jrx <jerome+git@rouaix.eu>
2023-12-08 02:04:54 +0100

    D7 refacto -> never again

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -1,12 +1,9 @@
-use std::cmp::Ordering;
+use std::{cmp::Ordering, marker::PhantomData};
 
 advent_of_code::solution!(7);
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-struct Play {
-  hand: Hand,
-  bid: u64,
-}
+use Card::*;
+use Strength::*;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 enum Card {
@@ -29,19 +26,19 @@ impl TryFrom<char> for Card {
   type Error = &'static str;
   fn try_from(value: char) -> Result<Self, Self::Error> {
     match value {
-      'A' => Ok(Self::_A),
-      'K' => Ok(Self::_K),
-      'Q' => Ok(Self::_Q),
-      'J' => Ok(Self::_J),
-      'T' => Ok(Self::_T),
-      '9' => Ok(Self::_9),
-      '8' => Ok(Self::_8),
-      '7' => Ok(Self::_7),
-      '6' => Ok(Self::_6),
-      '5' => Ok(Self::_5),
-      '4' => Ok(Self::_4),
-      '3' => Ok(Self::_3),
-      '2' => Ok(Self::_2),
+      'A' => Ok(_A),
+      'K' => Ok(_K),
+      'Q' => Ok(_Q),
+      'J' => Ok(_J),
+      'T' => Ok(_T),
+      '9' => Ok(_9),
+      '8' => Ok(_8),
+      '7' => Ok(_7),
+      '6' => Ok(_6),
+      '5' => Ok(_5),
+      '4' => Ok(_4),
+      '3' => Ok(_3),
+      '2' => Ok(_2),
       _ => Err("Invalid card"),
     }
   }
@@ -58,49 +55,57 @@ enum Strength {
   HighCard,
 }
 
-type InnerHand = [Card; 5];
+type Hand = [Card; 5];
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Ord)]
-struct Hand {
-  strength: Strength,
-  inner: InnerHand,
+trait Rule: Sized + std::fmt::Debug {
+  // fn ord(a: &Play<Self>, b: &Play<Self>) -> Ordering;
+  fn value(card: &Card) -> u64;
+  fn strength(h: Hand) -> Strength;
 }
 
-impl Hand {
-  fn new(inner: InnerHand) -> Self {
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct Play<R: Rule> {
+  _rule: PhantomData<R>,
+  hand: Hand,
+  strength: Strength,
+  bid: u64,
+}
+
+impl<R: Rule> Play<R> {
+  fn new(hand: Hand, bid: u64) -> Self {
+    let strength = R::strength(hand);
+    Self { hand, strength, bid, _rule: PhantomData }
+  }
+}
+
+#[derive(Debug)]
+struct P1;
+impl Rule for P1 {
+  fn value(card: &Card) -> u64 {
+    *card as u64
+  }
+
+  fn strength(hand: Hand) -> Strength {
     let mut counts = [0_usize; 13];
-    for card in inner.iter() {
+    for card in hand.iter() {
       counts[*card as usize] += 1;
     }
     counts.sort_unstable_by(|a, b| b.cmp(a));
 
-    let strength = match counts {
-      [5, ..] => Strength::FiveOfAKind,
-      [4, ..] => Strength::FourOfAKind,
-      [3, 2, ..] => Strength::FullHouse,
-      [3, ..] => Strength::ThreeOfAKind,
-      [2, 2, ..] => Strength::TwoPairs,
-      [2, ..] => Strength::OnePair,
-      _ => Strength::HighCard,
-    };
-
-    Self { strength, inner }
-  }
-}
-
-impl PartialOrd for Hand {
-  fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-    if self.strength != other.strength {
-      return self.strength.partial_cmp(&other.strength);
+    match counts {
+      [5, ..] => FiveOfAKind,
+      [4, ..] => FourOfAKind,
+      [3, 2, ..] => FullHouse,
+      [3, ..] => ThreeOfAKind,
+      [2, 2, ..] => TwoPairs,
+      [2, ..] => OnePair,
+      _ => HighCard,
     }
-    std::iter::zip(self.inner.iter(), other.inner.iter())
-      .find_map(|(a, b)| a.partial_cmp(b))
-      .or_else(|| Some(Ordering::Equal))
   }
 }
 
-fn get_inner_hand<T: TryFrom<char>>(hand: &str) -> Option<[T; 5]> {
-  let mut cards = hand.chars().filter_map(|c| T::try_from(c).ok());
+fn get_inner_hand(hand: &str) -> Option<Hand> {
+  let mut cards = hand.chars().filter_map(|c| Card::try_from(c).ok());
   let (Some(c1), Some(c2), Some(c3), Some(c4), Some(c5)) = (cards.next(), cards.next(), cards.next(), cards.next(), cards.next()) else {
     unimplemented!("Invalid hand: {hand}");
   };
@@ -108,86 +113,35 @@ fn get_inner_hand<T: TryFrom<char>>(hand: &str) -> Option<[T; 5]> {
 }
 
 pub fn part_one(input: &str) -> Option<u64> {
-  let mut plays: Vec<Play> = input
-    .lines()
-    .filter_map(|l| {
-      let mut parts = l.split_whitespace();
-      let (Some(hand), Some(bid)) = (parts.next(), parts.next()) else {
-        return None;
-      };
-
-      let inner = get_inner_hand(hand)?;
-      let hand = Hand::new(inner);
-      let bid = bid.parse().unwrap();
-
-      let play = Play { hand, bid };
-      Some(play)
-    })
-    .collect();
-
-  plays.sort_by(|a, b| b.hand.cmp(&a.hand));
-
-  Some(plays.iter().enumerate().map(|(i, play)| (i as u64 + 1) * play.bid).sum())
+  compute::<P1>(input)
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-struct Play2 {
-  hand: Hand2,
-  bid: u64,
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-enum Card2 {
-  _A,
-  _K,
-  _Q,
-  _T,
-  _9,
-  _8,
-  _7,
-  _6,
-  _5,
-  _4,
-  _3,
-  _2,
-  _J,
-}
-
-impl TryFrom<char> for Card2 {
-  type Error = &'static str;
-  fn try_from(value: char) -> Result<Self, Self::Error> {
-    match value {
-      'A' => Ok(Self::_A),
-      'K' => Ok(Self::_K),
-      'Q' => Ok(Self::_Q),
-      'J' => Ok(Self::_J),
-      'T' => Ok(Self::_T),
-      '9' => Ok(Self::_9),
-      '8' => Ok(Self::_8),
-      '7' => Ok(Self::_7),
-      '6' => Ok(Self::_6),
-      '5' => Ok(Self::_5),
-      '4' => Ok(Self::_4),
-      '3' => Ok(Self::_3),
-      '2' => Ok(Self::_2),
-      _ => Err("Invalid card"),
+#[derive(Debug)]
+struct P2;
+impl Rule for P2 {
+  fn value(card: &Card) -> u64 {
+    match card {
+      _A => 0,
+      _K => 1,
+      _Q => 2,
+      _T => 3,
+      _9 => 4,
+      _8 => 5,
+      _7 => 6,
+      _6 => 7,
+      _5 => 8,
+      _4 => 9,
+      _3 => 10,
+      _2 => 11,
+      _J => 12,
     }
   }
-}
 
-type InnerHand2 = [Card2; 5];
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Ord)]
-struct Hand2 {
-  strength: Strength,
-  inner: InnerHand2,
-}
-
-impl Hand2 {
-  fn new(inner: InnerHand2) -> Self {
+  fn strength(hand: Hand) -> Strength {
     let mut counts = [0_usize; 13];
     let mut j_count = 0_usize;
-    for card in inner.iter() {
-      if *card == Card2::_J {
+    for card in hand.iter() {
+      if *card == _J {
         j_count += 1;
       } else {
         counts[*card as usize] += 1;
@@ -196,54 +150,45 @@ impl Hand2 {
 
     counts.sort_unstable_by(|a, b| b.cmp(a));
 
-    let strength = match (counts, j_count) {
-      ([5, ..], _) => Strength::FiveOfAKind,
+    match (counts, j_count) {
+      ([5, ..], _) => FiveOfAKind,
 
-      ([4, ..], 1..) => Strength::FiveOfAKind,
-      ([4, ..], 0..) => Strength::FourOfAKind,
+      ([4, ..], 1..) => FiveOfAKind,
+      ([4, ..], 0..) => FourOfAKind,
 
-      ([3, 2, ..], _) => Strength::FullHouse,
+      ([3, 2, ..], _) => FullHouse,
 
-      ([3, ..], 0) => Strength::ThreeOfAKind,
-      ([3, ..], 1) => Strength::FourOfAKind,
-      ([3, ..], 2) => Strength::FiveOfAKind,
+      ([3, ..], 0) => ThreeOfAKind,
+      ([3, ..], 1) => FourOfAKind,
+      ([3, ..], 2) => FiveOfAKind,
 
-      ([2, 2, ..], 0) => Strength::TwoPairs,
-      ([2, 2, ..], 1) => Strength::FullHouse,
+      ([2, 2, ..], 0) => TwoPairs,
+      ([2, 2, ..], 1) => FullHouse,
 
-      ([2, ..], 0) => Strength::OnePair,
-      ([2, ..], 1) => Strength::ThreeOfAKind,
-      ([2, ..], 2) => Strength::FourOfAKind,
-      ([2, ..], 3) => Strength::FiveOfAKind,
+      ([2, ..], 0) => OnePair,
+      ([2, ..], 1) => ThreeOfAKind,
+      ([2, ..], 2) => FourOfAKind,
+      ([2, ..], 3) => FiveOfAKind,
 
-      ([1, ..], 0) => Strength::HighCard,
-      ([1, ..], 1) => Strength::OnePair,
-      ([1, ..], 2) => Strength::ThreeOfAKind,
-      ([1, ..], 3) => Strength::FourOfAKind,
-      ([1, ..], 4) => Strength::FiveOfAKind,
+      ([1, ..], 0) => HighCard,
+      ([1, ..], 1) => OnePair,
+      ([1, ..], 2) => ThreeOfAKind,
+      ([1, ..], 3) => FourOfAKind,
+      ([1, ..], 4) => FiveOfAKind,
 
-      (_, 5) => Strength::FiveOfAKind,
+      (_, 5) => FiveOfAKind,
 
-      _ => panic!("I don't know what i'm doing !! : {inner:?}"),
-    };
-
-    Self { strength, inner }
-  }
-}
-
-impl PartialOrd for Hand2 {
-  fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-    if self.strength != other.strength {
-      return self.strength.partial_cmp(&other.strength);
+      _ => panic!("I don't know what i'm doing !! : {hand:?}"),
     }
-    std::iter::zip(self.inner.iter(), other.inner.iter())
-      .find_map(|(a, b)| a.partial_cmp(b))
-      .or_else(|| Some(Ordering::Equal))
   }
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-  let mut plays: Vec<Play2> = input
+  compute::<P2>(input)
+}
+
+fn compute<R: Rule>(input: &str) -> Option<u64> {
+  let mut plays: Vec<Play<R>> = input
     .lines()
     .filter_map(|l| {
       let mut parts = l.split_whitespace();
@@ -251,16 +196,26 @@ pub fn part_two(input: &str) -> Option<u64> {
         return None;
       };
 
-      let inner = get_inner_hand(hand)?;
-      let hand = Hand2::new(inner);
+      let hand = get_inner_hand(hand)?;
       let bid = bid.parse().unwrap();
 
-      let play = Play2 { hand, bid };
+      let play = Play::new(hand, bid);
       Some(play)
     })
     .collect();
 
-  plays.sort_by(|a, b| b.hand.cmp(&a.hand));
+  plays.sort_by(|a, b| {
+    if a.strength != b.strength {
+      b.strength.cmp(&a.strength)
+    } else {
+      std::iter::zip(a.hand.iter(), b.hand.iter())
+        .map(|(a, b)| (R::value(a), R::value(b)))
+        .map(|(a, b)| b.cmp(&a))
+        .find(|ord| *ord != Ordering::Equal)
+        .unwrap_or(Ordering::Equal)
+    }
+  });
+  // dbg!(&plays);
 
   Some(plays.iter().enumerate().map(|(i, play)| (i as u64 + 1) * play.bid).sum())
 }
@@ -290,7 +245,7 @@ QQQJA 483"##;
   #[test]
   fn test_part_two() {
     let result = part_two(&advent_of_code::template::read_file("inputs", DAY));
-    assert_eq!(result, Some(248642943));
+    assert_eq!(result, Some(248750248));
   }
 
   #[test]
